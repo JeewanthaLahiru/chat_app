1dad9fd20cfc3a2ef25885fd0effb75634795bbb
JeewanthaLahiru <jeewanthalahiru00@gmail.com>
2021-06-08 12:39:23 +0530

    added input fields for email and password

--- lib/views/signin.dart
+++ lib/views/signin.dart
@@ -13,6 +13,28 @@ class _SignInState extends State<SignIn> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: appBarMain(context),
+      body: Container(
+        padding: EdgeInsets.symmetric(horizontal: 24.0),
+        child: Column(
+          children: [
+            TextField(
+              decoration: InputDecoration(
+                hintText: "email"
+              ),
+            ),
+            TextField(
+              decoration: InputDecoration(
+                  hintText: "password"
+              ),
+            ),
+            SizedBox(height: 12.0,),
+            Container(
+              alignment: Alignment.centerRight,
+              child: Text("Forget Password?"),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
