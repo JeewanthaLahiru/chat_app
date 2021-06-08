1a26b2a1adbd1d93e63cbf36682f7b0bc0d7ccfd
JeewanthaLahiru <jeewanthalahiru00@gmail.com>
2021-06-08 18:46:01 +0530

    signup with firebase

--- lib/views/signup.dart
+++ lib/views/signup.dart
@@ -1,3 +1,4 @@
+import 'package:chat_app/methods.dart';
 import 'package:chat_app/views/signin.dart';
 import 'package:chat_app/views/widget.dart';
 import 'package:flutter/cupertino.dart';
@@ -14,6 +15,7 @@ class _SignUpState extends State<SignUp> {
   final TextEditingController _name = TextEditingController();
   final TextEditingController _email = TextEditingController();
   final TextEditingController _password = TextEditingController();
+  bool isLoading = false;
 
   @override
   Widget build(BuildContext context) {
@@ -21,91 +23,120 @@ class _SignUpState extends State<SignUp> {
 
 
     return Scaffold(
-      body: Column(
-        children: [
-          SizedBox(
-            height: size.height/20,
-          ),
-          Container(
-            alignment: Alignment.centerLeft,
-            width: size.width/1.1,
-            child: IconButton(
-                onPressed: (){},
-                icon: Icon(Icons.arrow_back_ios)
-            ),
-          ),
-          SizedBox(
-            height: size.height/50,
-          ),
-          Container(
-            width: size.width/1.3,
-            child: Text("Welcome", style: TextStyle(
-                fontSize: 28,
-                fontWeight: FontWeight.bold
-            ),),
-          ),
-          Container(
-            width: size.width/1.3,
-            child: Text("Sign up to continue", style: TextStyle(
-                fontSize: 24,
-                color: Colors.grey,
-                fontWeight: FontWeight.w500
-            ),),
-          ),
-          SizedBox(
-            height: size.height/10,
-          ),
-          Container(
-              width: size.width,
-              alignment: Alignment.center,
-              child: field(size, "Name", Icons.verified_user, _name)
-          ),
-          Padding(
-            padding: const EdgeInsets.only(top: 18.0),
+      body: isLoading?
+          Center(
             child: Container(
-              width: size.width,
-              alignment: Alignment.center,
-              child: field(size, "Email", Icons.email, _email),
+              height: size.height/20,
+              width: size.height/20,
+              child: CircularProgressIndicator(),
             ),
-          ),
-          Padding(
-            padding: const EdgeInsets.symmetric(vertical: 18.0),
-            child: Container(
-              width: size.width,
-              alignment: Alignment.center,
-              child: field(size, "Password", Icons.password, _password),
+          )
+          : SingleChildScrollView(
+        child: Column(
+          children: [
+            SizedBox(
+              height: size.height/20,
             ),
-          ),
-          GestureDetector(
-            onTap: (){},
-            child: Container(
-              height: size.height/ 14,
-              width: size.width/ 1.3,
-              decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(10),
-                  color: Colors.blue
+            Container(
+              alignment: Alignment.centerLeft,
+              width: size.width/1.1,
+              child: IconButton(
+                  onPressed: (){},
+                  icon: Icon(Icons.arrow_back_ios)
               ),
-              alignment: Alignment.center,
-              child: Text(
-                "Signup",
-                style: TextStyle(
-                    color: Colors.white,
-                    fontSize: 22
+            ),
+            SizedBox(
+              height: size.height/50,
+            ),
+            Container(
+              width: size.width/1.3,
+              child: Text("Welcome", style: TextStyle(
+                  fontSize: 28,
+                  fontWeight: FontWeight.bold
+              ),),
+            ),
+            Container(
+              width: size.width/1.3,
+              child: Text("Sign up to continue", style: TextStyle(
+                  fontSize: 24,
+                  color: Colors.grey,
+                  fontWeight: FontWeight.w500
+              ),),
+            ),
+            SizedBox(
+              height: size.height/10,
+            ),
+            Container(
+                width: size.width,
+                alignment: Alignment.center,
+                child: field(size, "Name", Icons.verified_user, _name)
+            ),
+            Padding(
+              padding: const EdgeInsets.only(top: 18.0),
+              child: Container(
+                width: size.width,
+                alignment: Alignment.center,
+                child: field(size, "Email", Icons.email, _email),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(vertical: 18.0),
+              child: Container(
+                width: size.width,
+                alignment: Alignment.center,
+                child: field(size, "Password", Icons.password, _password),
+              ),
+            ),
+            GestureDetector(
+              onTap: (){
+                if(_name.text.isNotEmpty &&
+                    _email.text.isNotEmpty &&
+                    _password.text.isNotEmpty
+                ){
+                  setState(() {
+                    isLoading = true;
+                  });
+                  createAccount(_name.text, _email.text, _password.text).then((user) {
+                        if(user != null){
+                          setState((){
+                            isLoading = false;
+                          });
+                          print("login successfull");
+                        }
+                  });
+                }else{
+                  print("please enter details");
+                }
+              },
+              child: Container(
+                height: size.height/ 14,
+                width: size.width/ 1.3,
+                decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(10),
+                    color: Colors.blue
+                ),
+                alignment: Alignment.center,
+                child: Text(
+                  "Signup",
+                  style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 22
+                  ),
                 ),
               ),
             ),
-          ),
-          SizedBox(
-            height: size.height/25,
-          ),
-          GestureDetector(
-            onTap: () => Navigator.of(context).push(MaterialPageRoute(builder: (_) => SignIn())),
-            child: Text(
-              "Already have an account? Log in here",
-              style: TextStyle(color: Colors.blue),
+            SizedBox(
+              height: size.height/25,
             ),
-          )
-        ],
+            GestureDetector(
+              onTap: () => Navigator.of(context).push(MaterialPageRoute(builder: (_) => SignIn())),
+              child: Text(
+                "Already have an account? Log in here",
+                style: TextStyle(color: Colors.blue),
+              ),
+            )
+          ],
+        ),
       ),
     );
   }
